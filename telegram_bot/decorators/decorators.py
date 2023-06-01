313e88113ddccdde725c4152be876a8287d0c422
kaifusa2000@gmail.com <Artemon6770@>
2023-06-02 01:20:13 +0300

    added @private_message decorator

--- telegram_bot/decorators/decorators.py
+++ telegram_bot/decorators/decorators.py
@@ -24,7 +24,10 @@ def check_super_admin(func: Callable) -> Callable:
 
 @logger.catch
 def private_message(func):
-    async def wrapper(message: types.Message, *args, **kwargs):
+
+    @wraps(func)
+    async def wrapper(*args, **kwargs) -> Any:
+        message: Message = args[0]
         if message.chat.type == "private":
-            await func(message, *args, **kwargs)
+            await func(*args, **kwargs)
     return wrapper
